af3fc8ab65d8431a4082fc200c4410454e496bc8
Alya Rees <152781761+AlyaRees@users.noreply.github.com>
2024-07-23 11:45:37 +0100

    Update models.py for linting purposes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
+# 
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -9,8 +10,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -22,10 +25,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-    validators=[
-        MaxValueValidator(2023),
-        MinValueValidator(2015)
-    ])
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
+    
     def __str__(self):
         return self.name
 # - Many-To-One relationship to Car Make model (One Car Make has many
